7d57d72a3ec29c9e1af091eff723919c805a9903
Sherif-Ahmed17 <sherifnaiem2000@gmail.com>
2022-06-26 01:19:43 +0200

    findOrCreateDocument function

--- server/server.js
+++ server/server.js
@@ -31,3 +31,15 @@ io.on("connection", socket => {
     })
     //console.log("connected")
 })
+
+
+async function findOrCreateDocument(id){
+    if (id == null) return
+
+    const document = await Document.findById(id)
+
+    if (document) return document
+
+    return await Document.create({ _id: id, data: defaultValue})
+
+}
