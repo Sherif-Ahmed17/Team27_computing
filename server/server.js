3ada96d2ee32d542542e41cd193642f62ebc8316
Andrewsaied <Undercenaa@gmail.com>
2022-06-26 01:18:31 +0200

    GET-SAVE-RECIEVE Sockets

--- server/server.js
+++ server/server.js
@@ -9,3 +9,25 @@ const io = require("socket.io")(process.env.PORT , {
         methods: ["GET", "POST"],
     },
 })
+const defaultValue = ""
+
+io.on("connection", socket => {
+
+    socket.on("get-document", async documentId => {
+
+        const document = await findOrCreateDocument(documentId)
+        socket.join(documentId)
+        socket.emit("load-document", document.data)
+
+        socket.on('send-changes', delta => {
+            socket.broadcast.to(documentId).emit("receive-changes", delta) 
+        })
+
+    socket.on("save-document", async data => {
+
+        await Document.findByIdAndUpdate(documentId, { data })
+    })
+
+    })
+    //console.log("connected")
+})
